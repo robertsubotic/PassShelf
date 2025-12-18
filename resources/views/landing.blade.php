e3fba9b87afd20c07408c05bbc0b5bd9246e3022
Robert Subotic <robisubotic2005@gmail.com>
2025-12-18 12:25:34 +0100

    Add logo/favicon

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -8,7 +8,7 @@
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="flex justify-between h-16">
                     <div class="flex items-center">
-                        <span class="text-2xl font-bold text-emerald-500">PassShelf</span>
+                        <span class="cursor-pointer text-2xl font-bold text-emerald-500 flex items-center"><img src="/logo.png" class="w-20 h-20 rounded-4xl" />PassShelf</span>
                     </div>
 
                     <div class="flex items-center">
